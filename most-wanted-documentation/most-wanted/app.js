f0b86967938e6df8b2abd19b0ec5c0f5a29c1c7c
Devin Chen <taunwe79@gmail.com>
2022-07-28 11:37:45 -0500

    added alert framework for family display and added siblings function to display all applicable siblings

--- most-wanted-documentation/most-wanted/app.js
+++ most-wanted-documentation/most-wanted/app.js
@@ -31,17 +31,8 @@ function app(people) {
         case "no":
             //! TODO #4: Declare a searchByTraits (multiple traits) function //////////////////////////////////////////
                 //! TODO #4a: Provide option to search for single or multiple //////////////////////////////////////////
-        function searchByTraits(){
-            let userTraits = promptFor("Would you like to search with single or multiple traits?\n\n Please type in search criteria without spaces then the value.\n Enter multiple traits as separated by a semicolon (no space around the semicolon).\n\n (Ex. Single Criteria: eyecolor brown)\n (Ex. Multiple Criteria: eyecolor brown;weight 175):").toLowerCase();
-            let traits = data.filter(function(people){
-                if(userTraits === "gender female");
-            });
-            return traits;
-        }
             searchResults = searchByTraits(people);
-            console.log(`The following individuals match the search criteria: ${firstName} ${lastName}`, searchResults);
             break;
-
         default:
             // Re-initializes the app() if neither case was hit above. This is an instance of recursion.
             app(people);
@@ -75,19 +66,14 @@ function mainMenu(person, people) {
         case "info":
             //! TODO #1: Utilize the displayPerson function //////////////////////////////////////////
             // HINT: Look for a person-object stringifier utility function to help
-
             let personInfo = displayPerson(person[0]);
             alert(personInfo);
-        
         break;
         case "family":
             //! TODO #2: Declare a findPersonFamily function //////////////////////////////////////////
             // HINT: Look for a people-collection stringifier utility function to help
-        
-        function findPersonFamily(){
             let personFamily = findPersonFamily(person[0], people);
             alert(personFamily);
-        }
             break;
         case "descendants":
             //! TODO #3: Declare a findPersonDescendants function //////////////////////////////////////////
@@ -154,7 +140,7 @@ function displayPeople(people) {
 function displayPerson(person) {
     let personInfo = `First Name: ${person.firstName}\n Last Name: ${person.lastName}\n Gender: ${person.gender}\n Date of Birth: ${person.dob}\n Height: ${person.height}\n Weight: ${person.weight}\n Eye Color: ${person.eyeColor}\n Occupation: ${person.occupation}\n`
 
-    //! TODO #1a: finish getting the rest of the information to display //////////////////////////////////////////
+    //! TODO #1a: finish getting the rest of the information to display ////////////////////////////////////////// (DONE)
     alert(personInfo);
 }
 // End of displayPerson()
@@ -198,3 +184,18 @@ function chars(input) {
 
 //////////////////////////////////////////* End Of Starter Code *//////////////////////////////////////////
 // Any additional functions can be written below this line 👇. Happy Coding! 😁
+
+// Function to return inputted user siblings 
+
+function siblingFilter(person, people){
+    let hasSiblings = people.filter(function(siblingName){
+        let bothHaveCommonParents = false;
+        for(let i = 0; i < siblingName.parents.length; i++){
+            if(person.parents.includes(siblingName.parents[i])){
+                bothHaveCommonParents = true;
+            }
+        }
+        return bothHaveCommonParents;
+    });
+    return hasSiblings;
+}
--- most-wanted-documentation/most-wanted/main.js
+++ most-wanted-documentation/most-wanted/main.js
@@ -6,7 +6,7 @@
 
 // · You should be able to find and return a list of people who match the search
 
-// (10 points): As a user, I want to be able to look up someone’s information after I find them with the program (display values for the various traits of the found person).
+// (10 points): As a user, I want to be able to look up someone’s information after I find them with the program (display values for the various traits of the found person). (DONE)
 
 // (15 points): As a user, after locating a person, I want to see only that person’s immediate family members, displaying the names of the family members and their relation to the found person.
 
